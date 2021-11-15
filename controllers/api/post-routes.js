de3d5a157fefbf1e3decb782dd082169a74cc2e6
AS <asadrgilany7@gmail.com>
2021-11-15 13:56:17 -0500

    updated single post with comment/upvote functionality and added if statements to handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -100,12 +100,14 @@ router.post("/", (req, res) => {
 
 // track a post's votes
 router.put("/upvote", (req, res) => {
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
+    if (req.session) {
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
         });
+    }
 });
 
 // update a post's title
